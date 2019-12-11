75c164b8d7731ade5651a48ccca29276328ad9eb
wanggaoliang <wanggaoliang@xueleyun.com>
2019-12-11 19:32:12 +0800

    value注解的使用

---
+++ src/main/java/com/example/bootFight/service/DemoSingletonService.java
@@ -0,0 +1,12 @@
+package com.example.bootFight.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: wgl
+ * @Date: 2019/12/11 11:17
+ * @Description:
+ */
+@Service
+public class DemoSingletonService {
+}
---
+++ src/main/java/com/example/bootFight/service/ScopeConfig.java
@@ -0,0 +1,14 @@
+package com.example.bootFight.service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: wgl
+ * @Date: 2019/12/11 11:19
+ * @Description:
+ */
+@Configuration
+@ComponentScan("com.example.bootFight.service")
+public class ScopeConfig {
+}
